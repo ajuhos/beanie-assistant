89b4155ba32c22e3d0968d06c77d3a4550c1d23e
Juhos Ádám <ajuhos@logiscool.com>
2018-10-20 15:00:56 +0200

    Add executor for currency exchange intent

--- src/components/CommandExecutor.ts
+++ src/components/CommandExecutor.ts
@@ -1,4 +1,5 @@
 import {CommandResult} from "./CommandProcessor";
+const request = require('request-promise-native');
 
 export type CommandResponse = {
     command: CommandResult,
@@ -14,7 +15,29 @@ export class CommandExecutor {
 
     private executors: { [key: string]: (command: CommandResult) => Promise<CommandResponse> } = {
 
-        "currency_exchange": async (command) => ({ command, kind: 'beep', value: 'success' })
+        "currency_exchange": async (command) => {
+            const to = command.parameters['currency-name'];
+
+            const response = await request({
+                uri: 'http://data.fixer.io/api/convert',
+                qs: {
+                    access_key: "0cd2f839625accff168ebd8c63a1110f",
+                    from: command.parameters['unit-currency']['currency'],
+                    to,
+                    amount: command.parameters['unit-currency']['amount']
+                },
+                json: true
+            });
+
+            console.log(response);
+
+            if(response.success) {
+                return {command, kind: 'speech', value: `It is ${Math.round(response.result)} ${to}`}
+            }
+            else {
+                return {command, kind: 'beep', value: 'failure' }
+            }
+        }
 
     };
 
