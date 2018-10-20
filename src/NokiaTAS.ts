f9327b613e9c7e5a5fe195ddb386262ff8e88b0c
Juhos Ádám <ajuhos@logiscool.com>
2018-10-20 17:36:54 +0200

    Add NOKIA TAS integration
    Automatically play birthday reminder when Panna is called.

---
+++ src/NokiaTAS.ts
@@ -0,0 +1,57 @@
+import {ResponseExecutor} from "./components";
+
+const bodyParser = require('body-parser'),
+    express = require('express');
+
+export class NokiaTAS {
+
+    private app: any;
+    private responder = new ResponseExecutor;
+
+    constructor() {
+        this.app = express();
+        this.app.use(bodyParser.json());
+
+        this.app.use('/sounds', express.static('sounds'));
+
+        this.app.post('/', async (req, res) => {
+            const session = req.body;
+
+          /*  await this.responder.executeResponse({
+                kind: 'speech',
+                value: 'The person you are calling has birthday this week. I am starting the call now.',
+                command: null
+            });
+
+            res.send({
+                "action": {
+                    "actionToPerform": "Continue",
+                    "displayAddress": session.callEventNotification.callingParticipant
+                }
+            });*/
+
+            console.log("Playing birthday reminder...");
+
+            res.send({
+                "action": {
+                    "actionToPerform": "Continue",
+                    "displayAddress": session.callEventNotification.calledParticipant,
+                    "digitCapture": {
+                        "playingConfiguration": {
+                            "playFileLocation": "https://f0bc3ace.ngrok.io/sounds/birthday_long.wav"
+                        },
+                        "callParticipant": [
+                            session.callEventNotification.callingParticipant
+                        ]
+                    }
+                }
+            })
+        })
+
+    }
+
+    listen() {
+        this.app.listen(3333)
+    }
+
+}
