d16bceba33015a17a9fe9bacf54d2970c1ef9e5a
Balázs Sági <balazssagi@outlook.com>
2018-10-21 11:36:01 +0200

    fix get_weathar executor

--- src/components/CommandExecutor.ts
+++ src/components/CommandExecutor.ts
@@ -92,7 +92,7 @@ export class CommandExecutor {
             return {command, kind: 'speech', value: `Created event ${location} ${date}`}
         },
 
-        "end_call": async command => {
+        "end_calla": async command => {
             if(!this.callSession) return { command, kind: 'beep', value: 'failure' };
             await request.post({
                 uri: 'https://mn.developer.nokia.com/callback/endCallCalled',
@@ -107,21 +107,32 @@ export class CommandExecutor {
             return { command, kind: 'beep', value: 'success' }
         },
 
-        "get_weather": async command => {
+        "end_call": async command => {
             try {
                 const apiKey = 'AJ7A9GDgxRQoUGU1C70ekT8m5r9dMDzQ'
                 const city = command.parameters['geo-city'] || 'Budapest'
+                const date = command.parameters['date']
                 const cityRes = await request.get({
                     uri: `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`,
                     json: true
                 })
                 const cityId = cityRes[0].Key
-                const weatherRes = await request.get({
-                    uri: `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${cityId}?apikey=${apiKey}`,
-                    json: true
-                })
-                const weatherText = weatherRes.DailyForecasts[0].Day.IconPhrase
-                return { command, kind: 'speech', value: `Weather for tomorrow in ${city}: ${weatherText}` }
+                if (date) {
+                    const weatherRes = await request.get({
+                        uri: `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${cityId}?apikey=${apiKey}`,
+                        json: true
+                    })
+                    const weatherText = weatherRes.DailyForecasts[0].Day.IconPhrase
+                    return { command, kind: 'speech', value: `The weather tomorrow in ${city} will be ${weatherText}` }
+                }
+                else {
+                    const weatherRes = await request.get({
+                        uri: `http://dataservice.accuweather.com/currentconditions/v1/${cityId}?apikey=${apiKey}`,
+                        json: true
+                    })
+                    const weatherText = weatherRes[0].WeatherText
+                    return { command, kind: 'speech', value: `The current weather in ${city} is ${weatherText}` }
+                }
             }
             catch(e) {
 
